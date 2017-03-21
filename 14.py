e8f73cb4291901559d1763557872e1a68505f6a6
Víctor Manuel <victormanuelap@gmail.com>
2017-03-21 23:02:20 +0100

    Ejemplo de condicional if anidada en Python

---
+++ 14.py
@@ -0,0 +1,11 @@
+edad = 19
+pago=True
+
+if edad>18:
+    print('Sí es mayor de edad')
+    if pago==True:
+        print('Entonces eres mayor y pagaste')
+    else:
+        print('Eres mayor de edad y no pagaste')
+else:
+    print('No es mayor de edad')
