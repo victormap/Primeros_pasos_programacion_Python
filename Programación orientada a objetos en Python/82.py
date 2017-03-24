039b67cf1909b72eeaf80756de7480bdf51e092b
Víctor Manuel <victormanuelap@gmail.com>
2017-03-24 12:10:28 +0100

    Segundo ejemplo de Herencia en Python

---
+++ Programación orientada a objetos en Python/82.py
@@ -0,0 +1,29 @@
+class Persona:
+    nBrazos=0
+    nPiernas=0
+    cabello=True
+    cCabello="Defecto"
+    hambre=0
+
+    def __init__(self):
+        self.nBrazos=2
+        self.nPiernas=2
+
+    def dormir():
+        pass
+    def comer(self):
+        self.hambre=0
+
+class Hombre(Persona):
+    nombre="Defecto"
+    sexo="M"
+    def cambiarNombre(self,nombre):
+        self.nombre=nombre
+
+class Mujer(Persona):
+    nombre="Defecto"
+    sexo="F"
+
+jose=Hombre()
+jose.comer()
+print(jose.hambre)
