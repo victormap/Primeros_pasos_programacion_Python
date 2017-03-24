29609a1cd0c84d415a0b12a7f6882465d125c78c
Víctor Manuel <victormanuelap@gmail.com>
2017-03-24 11:49:59 +0100

    Ejemplo de definición de atributos en Python

---
+++ Programación orientada a objetos en Python/78.py
@@ -0,0 +1,13 @@
+class Persona:
+    nBrazos=0
+    nPiernas=0
+    cabello=True
+    cCabello="Defecto"
+
+class Hombre:
+    nombre="Defecto"
+    sexo="M"
+
+class Mujer:
+    nombre="Defecto"
+    sexo="F"
