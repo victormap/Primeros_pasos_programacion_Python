a5e01945eecf1c2f35b9ab91ac77b967529800ec
Víctor Manuel <victormanuelap@gmail.com>
2017-03-24 12:05:48 +0100

    Ejemplo de creación del constructor de una clase

---
+++ Programación orientada a objetos en Python/80.py
@@ -0,0 +1,25 @@
+class Persona:
+    nBrazos=0
+    nPiernas=0
+    cabello=True
+    cCabello="Defecto"
+    hambre=0
+
+    def __init__(self):
+        self.nBrazos=2
+        self.nPiernas=2
+
+    def dormir():
+        pass
+    def comer(self):
+        self.hambre=0
+
+class Hombre:
+    nombre="Defecto"
+    sexo="M"
+    def cambiarNombre(self,nombre):
+        self.nombre=nombre
+
+class Mujer:
+    nombre="Defecto"
+    sexo="F"
